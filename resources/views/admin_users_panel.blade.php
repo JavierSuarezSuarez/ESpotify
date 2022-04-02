a58412cfee4426b240fae976ee1e8a9ee1cd8f08
JavierSuarezSuarez <jasua2000@gmail.com>
2022-04-02 13:38:50 +0100

    Version 1.0.2 Template Usuarios, Routing y Modif temp carpeta css

--- resources/views/admin_users_panel.blade.php
+++ resources/views/admin_users_panel.blade.php
@@ -1,10 +1,126 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="{{ asset('css/users_panel_style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/headers.css') }}" rel="stylesheet">
+        <!--<script src="../js/jquery-3.6.0.min.js"></script>-->
+        <title>Panel de Usuarios</title>
+    </head>
 
-</body>
+    <body>
+        <!-- Header Section -->
+        <header class="headers">
+            <img class="logo" src="images/logo.jpg">
+        </header>
+
+        <hr class="hr">
+
+        <!-- Users - Browser - Add User Subheader Section -->
+        <section class="subheaders_section">
+
+            <header class="subheaders">
+                <h2 class="subheaders_titles"> Usuarios </h2>
+            </header>
+
+            <div class="subheaders_browser_div">
+                <input class="browser_input" type="text" placeholder="Search...">
+            </div>
+
+            <div class="subheaders_btn_div">
+                <img class="subheaders_btn_image" src="images/addbtn.jpg" title="Add Button Image" alt="Add Button Image">
+                <a class="subheaders_link" href="#">A&ntilde;adir Usuario </a>
+            </div>
+
+        </section>
+
+
+
+        <!-- Users Section -->
+
+        <section class="users_section">
+
+            <div class="user_div">
+                <img class="user_img" title="User Image" alt="User Image" src="images/profile.png">
+                <p class="user_name_text">PEPE MORENO P&Eacute;REZ</p>
+
+                <div class="edit_delete_btns">
+                    <button class="edit_btn">
+                        <img  class="edit_btn_img" title="Edit" alt="Edit" src="images/editbtn.png">
+                    </button>
+
+                    <button class="delete_btn" type="button">
+                        <img class="delete_btn_img" title="Delete" alt="Delete" src="images/deletebtn.png">
+                    </button>
+                </div>
+            </div>
+
+            <div class="user_div">
+                <img class="user_img" title="User Image" alt="User Image" src="images/profile.png">
+                <p class="user_name_text">PEPE MORENO P&Eacute;REZ</p>
+
+                <div class="edit_delete_btns">
+                    <button class="edit_btn">
+                        <img  class="edit_btn_img" title="Edit" alt="Edit" src="images/editbtn.png">
+                    </button>
+
+                    <button class="delete_btn" type="button">
+                        <img class="delete_btn_img" title="Delete" alt="Delete" src="images/deletebtn.png">
+                    </button>
+                </div>
+            </div>
+
+            <div class="user_div">
+                <img class="user_img" title="User Image" alt="User Image" src="images/profile.png">
+                <p class="user_name_text">PEPE MORENO P&Eacute;REZ</p>
+
+                <div class="edit_delete_btns">
+                    <button class="edit_btn">
+                        <img  class="edit_btn_img" title="Edit" alt="Edit" src="images/editbtn.png">
+                    </button>
+
+                    <button class="delete_btn" type="button">
+                        <img class="delete_btn_img" title="Delete" alt="Delete" src="images/deletebtn.png">
+                    </button>
+                </div>
+            </div>
+
+            <div class="user_div">
+                <img class="user_img" title="User Image" alt="User Image" src="images/profile.png">
+                <p class="user_name_text">PEPE MORENO P&Eacute;REZ</p>
+
+                <div class="edit_delete_btns">
+                    <button class="edit_btn">
+                        <img  class="edit_btn_img" title="Edit" alt="Edit" src="images/editbtn.png">
+                    </button>
+
+                    <button class="delete_btn" type="button">
+                        <img class="delete_btn_img" title="Delete" alt="Delete" src="images/deletebtn.png">
+                    </button>
+                </div>
+            </div>
+
+            <div class="user_div">
+                <img class="user_img" title="User Image" alt="User Image" src="images/profile.png">
+                <p class="user_name_text">PEPE MORENO P&Eacute;REZ</p>
+
+                <div class="edit_delete_btns">
+                    <button class="edit_btn">
+                        <img  class="edit_btn_img" title="Edit" alt="Edit" src="images/editbtn.png">
+                    </button>
+
+                    <button class="delete_btn" type="button">
+                        <img class="delete_btn_img" title="Delete" alt="Delete" src="images/deletebtn.png">
+                    </button>
+                </div>
+            </div>
+
+
+        </section>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+
+Route::get('/', function () {
+    return view('admin_users_panel');
 });
