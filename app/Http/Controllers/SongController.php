33fd4a29e72adb247c84c6e8102157f661c86447
JavierSuarezSuarez <jasua2000@gmail.com>
2022-04-14 15:45:13 +0100

    Version 1.1.2 Creacion controladores, request y modelo Playlist

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSongRequest;
+use App\Http\Requests\UpdateSongRequest;
+use App\Models\Song;
+
+class SongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSongRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSongRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Song $song)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Song $song)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSongRequest  $request
+     * @param  \App\Models\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSongRequest $request, Song $song)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Song  $song
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Song $song)
+    {
+        //
+    }
+}
