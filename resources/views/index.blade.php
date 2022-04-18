6f3876d998ab665bba01e5d36afa6053678ff82d
Natanael Martínez Morales <natanaelmartinezmorales@MacBook-Pro-de-Natanael.local>
2022-04-18 10:26:33 +0100

    formulario admin para añadir y/o modificar canciones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,35 @@
 @extends('layouts/general-layout')
 
 @section('mainContent')
-    He entrado
-
+    <div class="d-flex justify-content-center">
+        <form class="login-form border rounded p-5 bg-light mt-4 mb-4">
+            <h1 class="text-center">Create/Modify Song</h1>
+            <label for="name" class="mt-2">Nombre</label>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="">
+            </div>
+            <label for="apellidos">Apellidos</label>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="">
+            </div>
+            <label for="email">Email</label>
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="">
+            </div>
+            <label for="password">Contraseña</label>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="">
+            </div>
+            <label for="foto">Foto</label>
+            <div class="custom-file container">
+                <input type="file" class="custom-file-input" id="customFile">
+                <label class="custom-file-label" for="customFile">Seleccionar foto</label>
+            </div>
+            <div class="form-check mt-4">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">Admin</label>
+            </div>
+            <button type="button" class="btn save-btn mt-4">Save</button>
+        </form>
+    </div>
 @endsection
--- resources/views/layouts/general-layout.blade.php
+++ resources/views/layouts/general-layout.blade.php
@@ -5,10 +5,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+
         <!--==================== BOOTSTRAPRAP ====================-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!--==================== UNICONS ====================-->
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+        <link rel="stylesheet" href="{{ asset('css/admin-forms.css') }}">
+
 
         <title>Document</title>
     </head>
