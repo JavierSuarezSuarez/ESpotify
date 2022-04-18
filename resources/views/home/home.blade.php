cc0ea3aa635026d0af697aca3144fcfb6212759d
joaquinJPB <jjpb2000@gmail.com>
2022-04-18 19:30:38 +0100

    Views Home + Home unregistered

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,118 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{asset('css/home.css')}}" rel="stylesheet">
+    <title>Home</title>
+</head>
+
+<header>
+
+</header>
+
+<body>
+    <main class="main-home h-100 d-flex justify-content-end align-items-center">
+        <section class="d-flex flex-column me-5">
+            <div class="top-panel mb-5">
+                <div class="px-3 py-2 d-flex flex-row justify-content-center">
+                    <div>
+                        <h3 class="fw-bold mb-4">Top artistas</h3>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>The Weekend</h4>
+                            <img class="ms-5" src="{{asset('images/theweekend.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Ariana Grande</h4>
+                            <img src="{{asset('images/ariana.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Post Malone</h4>
+                            <img src="{{asset('images/malone.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Lil Nax X</h4>
+                            <img src="{{asset('images/lilnasx.png')}}">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <img class="logo mx-4" src="{{asset('images/logo.png')}}">
+                    </div>
+                    <div>
+                        <h3 class="fw-bold mb-4">Top canciones</h3>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Save your tears</h4>
+                            <img class="ms-4" src="{{asset('images/tears.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>God is a woman</h4>
+                            <img src="{{asset('images/godisawoman.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Motley Crew</h4>
+                            <img src="{{asset('images/crew.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Industry Baby</h4>
+                            <img src="{{asset('images/industrybaby.png')}}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="fav-panel">
+                <div class="px-3 py-2 d-flex flex-row justify-content-between">
+                    <div>
+                        <h3 class="fw-bold mb-4">Fav artistas</h3>
+                        <div class="d-flex flex-row justify-content-between mb-2 mb-2">
+                            <h4>The Weekend</h4>
+                            <img class="ms-5" src="{{asset('images/theweekend.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2 mb-2">
+                            <h4>Ariana Grande</h4>
+                            <img src="{{asset('images/ariana.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2 mb-2">
+                            <h4>Post Malone</h4>
+                            <img src="{{asset('images/malone.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2 mb-2">
+                            <h4>Lil Nax X</h4>
+                            <img src="{{asset('images/lilnasx.png')}}">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <img class="logo mx-4" src="{{asset('images/logo.png')}}">
+                    </div>
+                    <div>
+                        <h3 class="fw-bold mb-4">Fav canciones</h3>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Save your tears</h4>
+                            <img class="ms-4" src="{{asset('images/tears.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>God is a woman</h4>
+                            <img src="{{asset('images/godisawoman.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Motley Crew</h4>
+                            <img src="{{asset('images/crew.png')}}">
+                        </div>
+                        <div class="d-flex flex-row justify-content-between mb-2">
+                            <h4>Industry Baby</h4>
+                            <img src="{{asset('images/industrybaby.png')}}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+<footer>
+
+</footer>
+
+<html/>
