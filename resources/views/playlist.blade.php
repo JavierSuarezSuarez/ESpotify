f36031563de511bab367bf9f7b6c60613d37ee69
joaquinJPB <jjpb2000@gmail.com>
2022-04-23 14:32:49 +0100

    View Playlist - Final View Responsive

--- resources/views/playlist.blade.php
+++ resources/views/playlist.blade.php
@@ -17,30 +17,32 @@
 
     </header>
 
-    <main class="main-playlist h-100 d-flex flex-column justify-content-center align-items-center">
+    <main class="main-playlist d-flex flex-column justify-content-center align-items-center">
         <section class="container d-flex flex-column justify-content-center">
-            <div class="d-flex flex-column flex-md-row mb-5 justify-content-between">
-                <div class="">
-                    <img src="{{asset('images/playlist_cover.png')}}">
+            <div class="d-flex flex-column flex-lg-row mb-3 justify-content-between">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{asset('images/playlist_cover.png')}}" class="w-100">
                 </div>
-                <div class="d-flex flex-column mt-5 me-5">
+                <div class="d-flex flex-column mt-3 mt-md-5 mx-lg-4">
                     <h2 class="playlist">Playlist</h2>
                     <h3 class="playlist-title">Sound of Street</h3>
-                    <div class="d-flex flex-row mt-5">
+                    <div class="d-flex flex-column flex-md-row mt-sm-2 mt-md-3 align-items-md-center">
                         <p>@pepemoreno</p>
-                        <p class="mx-2">* 50 canciones</p>
+                        <p class="mx-md-2">* 50 canciones</p>
                         <p>2h 58 min</p>
-                        <p class="d-flex flex-row jusify-content-center align-items-center"><button class="ms-4"><i class='bx bxs-plus-circle mx-2'></i>Añadir cancion</button></p>
+                        <p class="d-flex flex-row jusify-content-center align-items-center"><button class="ms-md-4 add-song"><i class='bx bxs-plus-circle mx-2'></i>Añadir cancion</button></p>
                     </div>
                 </div>
 
-                <div>
-                    <button class="follow-button">Seguir <i class='bx bx-plus'></i> </button>
+                <div class="d-flex align-items-start mt-3 mt-md-5">
+                    <div>
+                        <button class="follow-button">Seguir <i class='bx bx-plus'></i> </button>
+                    </div>
                 </div>
 
             </div>
 
-            <div class="">
+            <div class="table-div table-responsive">
                  <table class="table">
                     <thead>
                         <tr>
@@ -103,6 +105,45 @@
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+            <div class="cards-div d-none d-flex flex-column">
+                <div class="playlist-card d-flex flex-row justify-content-between">
+                    <div class="d-flex flex-row">
+                        <div>
+                            <p><strong>Thunder</strong></p>
+                            <p>Imagine Dragons</p>
+                        </div>
+                        <p>Evolve</p>
+                    </div>
+                    <div class="card-delete d-flex align-items-center px-4">
+                        <button><i class='bx bxs-x-circle playlist-icon'></i></button>
+                    </div>
+                </div>
+                <div class="playlist-card d-flex flex-row justify-content-between">
+                    <div class="d-flex flex-row">
+                        <div>
+                            <p><strong>Thunder</strong></p>
+                            <p>Imagine Dragons</p>
+                        </div>
+                        <p>Evolve</p>
+                    </div>
+                    <div class="card-delete d-flex align-items-center px-4">
+                        <button><i class='bx bxs-x-circle playlist-icon'></i></button>
+                    </div>
+                </div>
+                <div class="playlist-card d-flex flex-row justify-content-between">
+                    <div class="d-flex flex-row">
+                        <div>
+                            <p><strong>Thunder</strong></p>
+                            <p>Imagine Dragons</p>
+                        </div>
+                        <p>Evolve</p>
+                    </div>
+                    <div class="card-delete d-flex align-items-center px-4">
+                        <button><i class='bx bxs-x-circle playlist-icon'></i></button>
+                    </div>
+                </div>
             </div>
 
         </section>
